7aa2919cb505330a71b3d346b9be7f1e58693677
xVicArT <aristizabal9200@gmail.com>
2023-06-08 10:03:36 -0500

    Creacion de proyecto VITE-ReactJS + ANTD

---
+++ Proyectofinal/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias fugit tenetur doloribus possimus facere iste molestiae. Nihil commodi enim explicabo culpa expedita, at obcaecati magnam quaerat aliquid aperiam! Architecto, nihil.</p>
+    </>
+  )
+}
+
+export default App
